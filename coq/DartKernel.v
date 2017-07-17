45c500e6da0b8f772f2caa592781bbe5c77775d8
Zhivka Gucevska <zhivkag@google.com>
2017-07-17 17:09:01 +0200

    Modify abstract syntax for Dart Kernel

--- coq/DartKernel.v
+++ coq/DartKernel.v
@@ -1,72 +1,78 @@
-(** Abstract syntax Kernel language. *)
+Require Import String.
 
-Variable Identifier : Type.
+(** Abstract syntax Kernel *)
+Definition identifier : Type :=  string.
 Variable Member : Type.
-Variable Super : Type. 
 Variable Location : Type.
-Variable type : Type.
+Variable DartType : Type.
 
+(** *)
+Definition node_id : Type := nat.
 
 (** Expressions *)
+Inductive expr               : Type :=
+ | VarGet                    : var_declaration -> expr
+ | VarSet                    : var_declaration -> expr -> expr
+ | PropertyGet               : expr            -> identifier -> expr
+ | PropertySet               : expr            -> identifier -> expr -> expr
+ | DPropertyGet              : expr            -> Member -> expr
+ | DPropertySet              : expr            -> Member -> expr -> expr
+ | SuperGet                  : identifier      -> expr
+ | SuperSet                  : identifier      -> expr -> expr
+ | StaticGet                 : Member          -> expr
+ | StaticSet                 : Member          -> expr -> expr
+ | Not                       : expr            -> expr
+ | And                       : expr            -> expr
+ | Or                        : expr            -> expr
+ | Conditional               : expr            -> expr -> expr -> expr
+ | Is                        : expr            -> DartType -> expr
+ | As                        : expr            -> DartType -> expr
+ | This                      : expr
+ | Throw                     : expr            -> expr
+ | Rethrow                   : expr
+ | Await                     : expr            -> expr
+ | String                    : expr
+ | IntLit                    : expr
+ | DoubleLit                 : expr
+ | TrueLit                   : expr
+ | FalseLit                  : expr
+ | NullLit                   : expr
+ | Let                       : identifier      -> expr -> expr -> expr
+ | New                       : DartType        -> list expr -> expr
+ | StaticInvocation          : identifier      -> list expr -> expr
+ | InstanceMethodInvocation  : expr            -> identifier -> list expr -> expr
+ | DInstanceMethodInvocation : expr            -> Member -> list expr -> expr
+with
+ var_declaration             : Type :=
+  | VarDeclaration           : node_id         -> identifier -> expr -> var_declaration.
 
-Inductive expr : Type := 
- | VarrGet : Identifier -> expr
- | VarSet : Identifier -> expr -> expr
- | PropertyGet : expr -> Identifier -> expr
- | PropertySet : expr -> Identifier -> expr -> expr
- | DPropertyGet : expr -> Member -> expr
- | DPropertySet : expr -> Member -> expr -> expr
- | SuperGet : Super -> Identifier -> expr
- | SuperSet : Super -> Identifier -> expr -> expr
- | StaticGet : Member -> expr
- | StaticSet : Member -> expr -> expr
- | Not: expr -> expr
- | And: expr -> expr
- | Or: expr -> expr
- | Conditional: expr -> expr -> expr -> expr
- | Is: expr -> type -> expr
- | As: expr -> type -> expr
- | This: expr
- | Throw: expr -> expr
- | Rethrow: expr
- | Await: expr -> expr
- | String: expr
- | IntLit: expr
- | DoubleLit: expr
- | TrueLit: expr
- | FalseLit: expr
- | NullLit: expr
- | Let: Identifier -> expr -> expr -> expr
- | New: type -> list expr -> expr
- | StaticInvocation: Identifier -> list expr -> expr
- | InstanceMethodInvocation : expr -> Identifier -> list expr -> expr
- | DInstanceMethodInvocation : expr -> Member -> list expr -> expr.
 
 (** Statements *)
+
 Variable Label : Type.
 
 Inductive stmt : Type := 
- | Block : list stmt -> stmt
- | EStmt : expr -> stmt
- | Empty : stmt
- | Labeled : Label -> stmt -> stmt
- | Break : Label -> stmt
- | While : expr -> stmt -> stmt
- | Do : stmt -> expr -> stmt
- | For: list Identifier -> option expr -> list expr -> stmt -> stmt
- | Continue: stmt -> stmt
- | If: expr -> stmt -> stmt -> stmt
- | ReturnE : expr -> stmt
- | Return : stmt
- | TryCatch : stmt -> list expr -> list stmt -> stmt
- | TryFinally : stmt -> stmt -> stmt
- | Yield : expr -> stmt
- | VarDecl : Identifier -> expr -> stmt
- | FunDecl : type -> Identifier -> list type -> list Identifier -> stmt -> stmt.
+ | Block       : list stmt       -> stmt
+ | EStmt       : expr            -> stmt
+ | Empty       : stmt
+ | Labeled     : Label           -> stmt -> stmt
+ | Break       : Label           -> stmt
+ | While       : expr            -> stmt -> stmt
+ | Do          : stmt            -> expr -> stmt
+ | For         : list identifier -> option expr -> list expr -> stmt -> stmt
+ | Continue    : stmt            -> stmt
+ | If          : expr            -> stmt -> stmt -> stmt
+ | ReturnE     : expr            -> stmt
+ | Return      : stmt
+ | TryCatch    : stmt            -> list expr -> list stmt -> stmt
+ | TryFinally  : stmt            -> stmt -> stmt
+ | Yield       : expr            -> stmt
+ | VarDecl     : var_declaration -> stmt
+ | FunDecl     : DartType        -> identifier -> list DartType -> list identifier -> stmt -> stmt.
 
 (** Values *)
 
-Inductive Value : Set :=
+Inductive Value : Type :=
  | IntVal : nat -> Value
  | DoubleVal
  | StringVal
@@ -76,4 +82,15 @@ Inductive Value : Set :=
  | SymbolVal
  | NullVal.
 
-(**  *)
+
+(** Functions *)
+
+Variable Environment : Type.
+
+Record function : Type := mkfunction {
+                             f_formals : list var_declaration;
+                             f_env : Environment;
+                             f_body : stmt
+                           }.
+
+(** CESK machine *)
