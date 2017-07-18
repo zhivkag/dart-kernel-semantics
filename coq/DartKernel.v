be53c468af17f04b8a1198616e71133da07b02c4
Zhivka Gucevska <zhivkag@google.com>
2017-07-18 10:44:53 +0200

    Reformat

--- coq/DartKernel.v
+++ coq/DartKernel.v
@@ -10,11 +10,11 @@ Module Import M := FMapList.Make(Nat_as_OT).
 Definition identifier : Type :=  string.
 Definition location   : Type := nat.
 
-Inductive  member : Type :=
- | Field          : member
- | Method         : member
- | Getter         : member
- | Setter         : member.
+Inductive member : Type :=
+ | Field  : member
+ | Method : member
+ | Getter : member
+ | Setter : member.
 
 Variable DartType : Type.
 
@@ -24,23 +24,23 @@ Variable DartType : Type.
 Definition node_id : Type := nat.
 
 (** Expressions *)
-Inductive expr               : Type :=
+Inductive expr : Type :=
  | VarGet                    : var_declaration -> expr
- | VarSet                    : var_declaration -> expr -> expr
+ | VarSet                    : var_declaration -> expr       -> expr
  | PropertyGet               : expr            -> identifier -> expr
- | PropertySet               : expr            -> identifier -> expr -> expr
- | DPropertyGet              : expr            -> member -> expr
- | DPropertySet              : expr            -> member -> expr -> expr
+ | PropertySet               : expr            -> identifier -> expr      -> expr
+ | DPropertyGet              : expr            -> member     -> expr
+ | DPropertySet              : expr            -> member     -> expr      -> expr
  | SuperGet                  : identifier      -> expr
- | SuperSet                  : identifier      -> expr -> expr
+ | SuperSet                  : identifier      -> expr       -> expr
  | StaticGet                 : member          -> expr
- | StaticSet                 : member          -> expr -> expr
+ | StaticSet                 : member          -> expr       -> expr
  | Not                       : expr            -> expr
  | And                       : expr            -> expr
  | Or                        : expr            -> expr
- | Conditional               : expr            -> expr -> expr -> expr
- | Is                        : expr            -> DartType -> expr
- | As                        : expr            -> DartType -> expr
+ | Conditional               : expr            -> expr       -> expr      -> expr
+ | Is                        : expr            -> DartType   -> expr
+ | As                        : expr            -> DartType   -> expr
  | This                      : expr
  | Throw                     : expr            -> expr
  | Rethrow                   : expr
@@ -51,49 +51,49 @@ Inductive expr               : Type :=
  | TrueLit                   : expr
  | FalseLit                  : expr
  | NullLit                   : expr
- | Let                       : identifier      -> expr -> expr -> expr
- | New                       : DartType        -> list expr -> expr
- | StaticInvocation          : identifier      -> list expr -> expr
+ | Let                       : identifier      -> expr       -> expr      -> expr
+ | New                       : DartType        -> list expr  -> expr
+ | StaticInvocation          : identifier      -> list expr  -> expr
  | InstanceMethodInvocation  : expr            -> identifier -> list expr -> expr
- | DInstanceMethodInvocation : expr            -> member -> list expr -> expr
+ | DInstanceMethodInvocation : expr            -> member     -> list expr -> expr
 with
  var_declaration             : Type :=
- | VarDeclaration           : node_id         -> identifier -> expr -> var_declaration.
+ | VarDeclaration            : node_id         -> identifier -> expr      -> var_declaration.
 
 
 (** Statements *)
 
 Variable Label : Type.
 
-Inductive stmt : Type := 
- | Block       : list stmt       -> stmt
- | EStmt       : expr            -> stmt
- | Empty       : stmt
- | Labeled     : Label           -> stmt -> stmt
- | Break       : Label           -> stmt
- | While       : expr            -> stmt -> stmt
- | Do          : stmt            -> expr -> stmt
- | For         : list identifier -> option expr -> list expr -> stmt -> stmt
- | Continue    : stmt            -> stmt
- | If          : expr            -> stmt -> stmt -> stmt
- | ReturnE     : expr            -> stmt
- | Return      : stmt
- | TryCatch    : stmt            -> list expr -> list stmt -> stmt
- | TryFinally  : stmt            -> stmt -> stmt
- | Yield       : expr            -> stmt
- | VarDecl     : var_declaration -> stmt
- | FunDecl     : DartType        -> identifier -> list DartType -> list identifier -> stmt -> stmt.
+Inductive stmt : Type :=
+ | Block      : list stmt       -> stmt
+ | EStmt      : expr            -> stmt
+ | Empty      : stmt
+ | Labeled    : Label           -> stmt        -> stmt
+ | Break      : Label           -> stmt
+ | While      : expr            -> stmt        -> stmt
+ | Do         : stmt            -> expr        -> stmt
+ | For        : list identifier -> option expr -> list expr     -> stmt            -> stmt
+ | Continue   : stmt            -> stmt
+ | If         : expr            -> stmt        -> stmt          -> stmt
+ | ReturnE    : expr            -> stmt
+ | Return     : stmt
+ | TryCatch   : stmt            -> list expr   -> list stmt     -> stmt
+ | TryFinally : stmt            -> stmt        -> stmt
+ | Yield      : expr            -> stmt
+ | VarDecl    : var_declaration -> stmt
+ | FunDecl    : DartType        -> identifier  -> list DartType -> list identifier -> stmt -> stmt.
 
 
 (** Values *)
 
 Inductive value : Type :=
- | IntVal : nat -> value
+ | IntVal    : nat -> value
  | DoubleVal : value
  | StringVal
- | ObjectVal 
- | ListVal 
- | TypeVal 
+ | ObjectVal
+ | ListVal
+ | TypeVal
  | SymbolVal
  | NullVal.
 
